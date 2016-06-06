0ce56ba74d50620b573704debbe3ae2ed2488612
domlet <dombrassey@gmail.com>
2016-06-06 08:38:24 -0700

    take alias_manager.rb as far as I can for now

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,20 @@
+# pseudocode and write a method "Felicia Torres"
+# creates a fake name with it 
+# by doing the following:
+
+# Swap first and last name
+# Change all the vowels to the next vowel in 'aeiou' 
+# Change all of the consonants (everything else besides the vowels) 
+# to the next consonant in the alphabet. 
+# So 'a' would become 'e', 'u' would become 'a', and 'd' would become 'f'.
+
+
+def alias(x)
+  x.split(' ')
+end
+
+alias("hi")
+puts alias
+
+array.each {|x| puts "Hello #{x}"}
+p array
