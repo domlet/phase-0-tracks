80ba051a7e425609ba84a5b1b29ab233c2a1ab78
domlet <dombrassey@gmail.com>
2016-06-08 21:15:30 -0500

    initial work with pair on puppy_methods.rb but not finished yet

---
+++ ruby/puppy.rb
@@ -0,0 +1,21 @@
+# How might you design a Puppy class? 
+# What sorts of characteristics and behavior might a Puppy class involve? 
+# Using a format similar to the one we used for Eyelashus Adorableus, 
+# come up with some characteristics and behavior for a Puppy class. 
+# Use comments to add your class design to phase-0-tracks/ruby/puppy.rb.
+
+CLASS ------------------------
+Puppy
+
+CHARACTERISTICS ----------------
+Name: varies
+Breed: varies
+Friendly: yes
+Housetrained: no
+For sale: yes
+Weeks old: 8
+
+BEHAVIOR -----------------------
+Chew_toy
+Pee_on_floor
+Whimper
--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,5 +5,100 @@ class Puppy
     toy
   end
 
+# Add a speak method that takes an integer
+# and then prints "Woof!" that many times.
+
+  def speak(y)
+    x = 0
+    while x < y
+      puts "Woof!"
+      x += 1
+    end
+  end
+
+# Add a roll_over method that just prints "*rolls over*".
+
+  def roll_over
+    puts "*rolls over*"
+  end
+
+# Add a dog_years method that takes an integer (of human years) 
+# and converts that number to dog years, returning a new integer.
+
+  def dog_years(a)
+    dog_age = a * 7
+    puts dog_age
+  end
+
+# Add one more trick -- whichever one you'd like.
+
+  def destroy(item)
+    puts "You destroyed my #{item}! Bad dog. I love you."
+  end
+
+# Add a method to your Puppy class named initialize. 
+# It should print "Initializing new puppy instance ..."
+
+  def initialize
+     puts "Initializing new puppy instance ..."
+  end
+
 end
 
+# Add driver code at the bottom that initializes an instance of Puppy
+# Verify that your instance can now fetch a ball. 
+
+scooter = Puppy.new
+toy = "shoe"
+scooter.fetch(toy)
+
+scooter.speak(5)
+
+scooter.roll_over
+
+scooter.dog_years(5)
+
+scooter.destroy("laptop")
+
+
+# # # # # # # # # # # # # # # # # # 
+
+class Baby
+
+  def initialize
+    puts "Welcome, baby!"
+  end
+
+  def primal_scream
+    puts "AAAAAAHHHHHHH"
+  end
+
+  def diaper_change
+    if poop == true
+      puts "You gotta change that baby's diaper."
+    else
+      puts "This baby smells good."
+    end
+  end
+
+end
+
+# Testing the new class we created
+# Seth = Baby.new
+
+# Use a loop to make 50 instances of your class.
+
+
+# x = 1
+# while x < 5
+#   babies = [Baby.new(5)]
+#   x += 1
+#   p babies
+# end
+
+
+Baby.new
+
+
+# babies [Welcome, baby!, Welcome, baby!, Welcome, baby!, Welcome, baby!, Welcome, baby!]
+babies.each
