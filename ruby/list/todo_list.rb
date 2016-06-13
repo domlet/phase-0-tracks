626c1986282274e964ebacf2a2b1c962f91c3641
domlet <dombrassey@gmail.com>
2016-06-12 17:18:10 -0700

    complete 6.6 tests

--- ruby/list/todo_list.rb
+++ ruby/list/todo_list.rb
@@ -8,7 +8,15 @@ class TodoList
     @array
   end
 
-  def add_item
-    
+  def add_item(list_item)
+    @array << list_item
+  end
+
+  def delete_item(list_item)
+    @array.delete(list_item)
+  end
+
+  def get_item(i)
+    @array[i]
   end
 end
