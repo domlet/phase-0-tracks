ffe1458d214168feb1753769e60ba436f240808e
domlet <dombrassey@gmail.com>
2016-06-16 15:00:46 -0700

    add semi-colons

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,8 +1,8 @@
-var colors = ["red", "blue", "yellow", "green"]
-var horseNames = ["dom", "Kayden", "Amy", "Mike"]
+var colors = ["red", "blue", "yellow", "green"];
+var horseNames = ["dom", "Kayden", "Amy", "Mike"];
 
-colors.push("white")
-console.log(colors)
+colors.push("white");
+console.log(colors);
 
-horseNames.push("Scooby")
-console.log(horseNames)
+horseNames.push("Scooby");
+console.log(horseNames);
