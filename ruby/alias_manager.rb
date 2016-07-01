cdb7b434f53f54206c3c0a1d9fa576fc2c0a4375
domlet <dombrassey@gmail.com>
2016-07-01 14:52:03 -0700

    .next is working but need to except vowels.

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,20 +1,42 @@
 # pseudocode and write a method "Felicia Torres"
-# creates a fake name with it 
-# by doing the following:
+# creates a fake name by doing the following:
 
 # Swap first and last name
-# Change all the vowels to the next vowel in 'aeiou' 
-# Change all of the consonants (everything else besides the vowels) 
-# to the next consonant in the alphabet. 
-# So 'a' would become 'e', 'u' would become 'a', and 'd' would become 'f'.
+# Change vowels to the next vowel in 'aeiou' 
+# Change consonants to the next consonant in the alphabet. 
+
+# # # PSEUDOCODE # # #
+# create new array with values for each of 2 names
+# lowercase all letters
+# reverse the order of the 2 values
+# break each array into 2 hashes (consonants and values)
+# advance consonants and values as appropriate
+# capitalize first letters
+# compare result to "Vussit Gimodoe"
 
 
-def alias(x)
-  x.split(' ')
+def alias_manager(str)
+  orig_name = "#{str}".downcase
+  new_alias = []
+  new_alias = orig_name.split.reverse
+  p orig_name
+  p new_alias
+
+  name0 = []
+  name0 = new_alias[0].chars
+  p name0
+
+  name1 = []
+  name1 = new_alias[1].chars
+  p name1
+
+  p vowels = ["a","e","i","o","u"]
+  
+  name0.map! { |name0| name0.next}
+  name1.map! { |name0| name0.next}
+  p name0
+  p name1
+  
 end
 
-alias("hi")
-puts alias
-
-array.each {|x| puts "Hello #{x}"}
-p array
+alias_manager("Felicia Torres")
