09ba9a9425580bb2bc996c182709dd19b9c342c0
domlet <dombrassey@gmail.com>
2016-07-01 11:08:10 -0700

    good start on form

--- ruby/hashes_2.rb
+++ ruby/hashes_2.rb
@@ -1,1 +1,22 @@
- 
+ # enter the details of a given client: 
+# name
+# age
+# number of children
+# decor theme
+# vegan y/n
+# keys should be symbols.
+
+puts "Client name: "
+print "Age"
+puts "number of children"
+puts "decor theme"
+puts "vegan? y/n"
+
+# Your program should ...
+# Prompt the designer/user for all info
+# Convert user input to appropriate data type.
+# Print hash to screen when done.
+# Give the user the opportunity to update a key (no need to loop, once is fine). After all, sometimes users make mistakes! If the designer says "none", skip it. But if the designer enters "decor_theme" (for example), your program should ask for a new value and update the :decor_theme key. (Hint: Strings have methods that will turn them into symbols, which would be quite handy here.) You can assume the user will correctly input a key that exists in your hash -- no need to handle user errors.
+# Print the latest version of the hash, and exit the program.
+# Be sure to pseudocode, and leave your pseudocode in as comments.
+
