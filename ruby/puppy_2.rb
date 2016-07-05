4e157c37adffe3876e1d43a8895bf2600c988d2a
domlet <dombrassey@gmail.com>
2016-07-05 16:44:34 -0700

    Initial commit for puppy_2.rb

---
+++ ruby/puppy_2.rb
@@ -0,0 +1,15 @@
+# SPECIES ------------------------
+# Puppy
+
+# CHARACTERISTICS ----------------
+# Fur color: brown
+# Purebred: yes
+# Age: 1
+# Name: varies
+# Color: varies
+
+# BEHAVIOR -----------------------
+# Bark
+# Pee
+# Eat
+
