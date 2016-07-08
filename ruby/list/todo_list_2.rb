54c8298276ba26ca746f13ffcd2b43ad86431abe
domlet <dombrassey@gmail.com>
2016-07-08 14:31:18 -0700

    Add working driver code for 1st two rspec tests.

--- ruby/list/todo_list_2.rb
+++ ruby/list/todo_list_2.rb
@@ -1,6 +1,10 @@
 class TodoList
-  def initialize(list_array)
-    @list_array = list_array
+
+  def initialize(*args)
+    @list_array = []
+      args.each do |item| 
+      @list_array << item
+    end
   end  
 
   def get_items
@@ -12,6 +16,7 @@ class TodoList
   end
 
   def delete_item(item)
+    # item.to_i
     @list_array.delete(item)
   end
 
@@ -19,3 +24,23 @@ class TodoList
     @list_array[index]
   end
 end
+
+# stores the list items given on initialization
+dom_list = TodoList.new(1, 2)
+p dom_list
+
+# adds an item to the list
+dom_list.add_item("pick up Amazon package")
+p dom_list
+
+
+# deletes an item
+# dom_list.delete_item("1")
+# dom_list.delete_at([1])
+# p dom_list
+
+# retrieves an item by index
+
+# dom_list.get_item(1)
+# dom_list.get_items
+# p dom_list
