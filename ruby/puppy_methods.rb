922c61f4e798bfe6e2c055bc3c3954fd14e6d878
domlet <dombrassey@gmail.com>
2016-07-10 14:09:51 -0700

    Complete Release 0 for 6.2

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,12 +1,13 @@
 class Puppy
 
+  def initialize
+     puts "Initializing new puppy instance ..."
+  end
+
   def fetch(toy)
     puts "I brought back the #{toy}!"
     toy
   end
-
-# Add a speak method that takes an integer
-# and then prints "Woof!" that many times.
 
   def speak(y)
     x = 0
@@ -16,81 +17,67 @@ class Puppy
     end
   end
 
-# Add a roll_over method that just prints "*rolls over*".
-
   def roll_over
     puts "*rolls over*"
   end
-
-# Add a dog_years method that takes an integer (of human years) 
-# and converts that number to dog years, returning a new integer.
 
   def dog_years(a)
     dog_age = a * 7
     puts dog_age
   end
 
-# Add one more trick -- whichever one you'd like.
+# Add one more trick
 
   def destroy(item)
     puts "You destroyed my #{item}! Bad dog. I love you."
   end
 
-# Add a method to your Puppy class named initialize. 
-# It should print "Initializing new puppy instance ..."
-
-  def initialize
-     puts "Initializing new puppy instance ..."
-  end
-
 end
 
 # Add driver code at the bottom that initializes an instance of Puppy
-# Verify that your instance can now fetch a ball. 
-
 scooter = Puppy.new
-
+# Verify that your instance can now fetch a ball. 
 toy = "shoe"
 scooter.fetch(toy)
-
 scooter.fetch("pork chop")
-
 scooter.speak(5)
-
 scooter.roll_over
-
 scooter.dog_years(5)
-
 scooter.destroy("laptop")
 
+puts "\n # # # \n\n"
 
 # # # # # # # # # # # # # # # # # # 
-class Baby
-
-  def initialize
-    puts "Welcome, baby!"
-  end
-
-  def primal_scream
-    puts "AAAAAAHHHHHHH"
-  end
-
-  def diaper_change
-    if poop == true
-      puts "You gotta change that baby's diaper."
-    else
-      puts "This baby smells good."
-    end
-  end
-
-end
 
 
-5.times do 
-  Family = [] << Baby.new
-end
 
-p Family
+
+# class Baby
+
+#   def initialize
+#     puts "Welcome, baby!"
+#   end
+
+#   def primal_scream
+#     puts "AAAAAAHHHHHHH"
+#   end
+
+#   def diaper_change
+#     if poop == true
+#       puts "You gotta change that baby's diaper."
+#     else
+#       puts "This baby smells good."
+#     end
+#   end
+
+# end
+
+
+# 5.times do 
+#   Family = [] << Baby.new
+# end
+
+# p Family
 
 
 
