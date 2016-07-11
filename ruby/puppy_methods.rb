8a39ccd364745f610445433e0cb7b551d7339d4d
domlet <dombrassey@gmail.com>
2016-07-10 19:57:20 -0700

    Complete Q1 and Q2 for Release 2.

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -49,9 +49,46 @@ puts "\n # # # \n\n"
 
 # # # # # # # # # # # # # # # # # # 
 
+class Nachos
+
+def initialize
+  puts "Nachos on the way..."
+end
+
+def announce 
+  puts "Your nachos are ready!"
+end
+
+# def wrong_order(true_or_false)
+#   if order_is_wrong == true
+#     puts "This is not what I ordered. Can you correct my order?"
+#   elsif 
+#     puts "Thank you!"
+#   end
+# end
+      
+end
+
+order_421 = Nachos.new
+p order_421
+
+nacho_orders = []
+50.times do
+  nacho_orders << Nachos.new
+end
+p nacho_orders
+p nacho_orders[12]
 
 
+order_421.announce
 
+# nacho_orders.each { |i| announce }
+
+# order_is_wrong = true
+# order_421.wrong_order(true)
+
+# def assign_order_numbers(50)
+  
 # class Baby
 
 #   def initialize
