7133f3861994ed3a9255fd984494f09226e79962
domlet <dombrassey@gmail.com>
2016-07-25 20:08:56 -0700

    Add a caption to the DB.

--- databases/dom_brassey_instanalyzer/instanalyzer.rb
+++ databases/dom_brassey_instanalyzer/instanalyzer.rb
@@ -1,60 +1,83 @@
-# # require gems
-# require 'sqlite3'
+# require gems
+require 'sqlite3'
 
-# # create SQLite3 database
-# db = 
+# create SQLite3 database
+db = SQLite3::Database.new("captions.db")
+db.results_as_hash = false
 
-# create_database = CREATE TABLE IF NOT EXISTS (
-#   id = INTEGER PRIMARY KEY,
-#   text_entry = VARCHAR(255)
-#   hash)
+# add method using string delimiters
+create_table_cmd = <<-SQL
+  CREATE TABLE IF NOT EXISTS captions (
+    id INTEGER PRIMARY KEY,
+    caption VARCHAR(255),
+    words VARCHAR(255),
+    tags VARCHAR(255),
+    words_ct INT,
+    tags_ct INT)
+SQL
 
-class Instanalyzer
+# create a captions table
+db.execute(create_table_cmd)
 
-  attr_accessor :user_entry, :word_count_array, :tag_count_array
+# add a test kitten
+# db.execute("INSERT INTO captions (caption, words_ct) VALUES ('Bob hi', 100)")
 
-  def initialize
-    puts "Initializing new Instanalyzer..."
-    @user_entry = user_entry
-    # @user_entry_array = user_entry_array
-    @word_count_array = word_count_array
-    @tag_count_array = tag_count_array
-    # @word_ct = word_ct
-    # @tags_ct = tag_ct
-  end
-
-  def take_user_entry
-    puts "Paste your Instagram caption here:"
-    user_entry = gets.chomp
-    user_entry_array = user_entry.split 
-    p user_entry_array
-  end
-
-  def word_count(text)
-    word_count_array = text.split
-    word_ct = word_count_array.length
-    puts "Words: #{word_ct}"
-  end
-
+# PRINT ROWS AFTER X
+p rows = db.execute("SELECT * from captions WHERE id < 20")
+puts "\n"
+rows.each do |caption, words, tags, words_ct, tags_ct|
+  puts "
+    caption = #{caption} 
+    words = #{words}
+    tags = #{tags}
+    words_ct = #{words_ct}
+    tags_ct = #{tags_ct}
+"
 end
+puts "\n"
 
-# DRIVER CODE
 
-test = Instanalyzer.new
-test.take_user_entry
-# user_entry.word_count
-# p test
+# update a value
+# db.execute("SET caption = 'John' WHERE tags_ct='6'")
 
-# user_entry = "The #WeAreOrlando shooting has moved Latinx/Black-focused #techdiversity and workforce development pioneer @code2040 to double down on #LGBTQ inclusion and support. I read in the news that one of the families of the victims at first refused to claim his body. Shame. Do you have any family you want to leave on the bathroom floor? I don’t.
-# I walk through the streets of the #Tenderloin in #SanFrancisco. We got murals. But there are unclaimed bodies here, too. Are these your 'neighbors'? Diabetics with their feet rotting off. The unconscious derelict slumped in defecate. Contaminated, contagious people. Stuttering lunatics and howling ghosts. 
-# We walk by the living. We pay money to watch #TheWalkingDead. We give guns to police because we want someone to wash our sins from the Earth. Every great wall is a monument to what we are willing to sacrifice for wealth.
-# #endwhitesilence #gentrification #homelessness #economicjustice #zombies #healthcare #communityorganizing #diversityintech #POCintech #LGBT #androgynous #queer #softbutch #femalemasculinity #feminism #tomboy #boi #lesbian #queersofig #dapperQ #genderfluid #sartorial #ootd #wiwt"
+  def word_count(db, caption)
+    user_input = caption #string
+    word_count_array = caption.split #array
+    words_ct = word_count_array.length #int (count the words)
+    tag_count_array = word_count_array.select  { |word| word[0, 1] == "#" } 
+    tags_ct = tag_count_array.length
+    puts "Words: #{words_ct}"
+    puts "Tags: #{tags_ct}"
+    db.execute("INSERT INTO captions (caption) VALUES (?)", [caption])
+  end
 
-# count words
 
-# count tags
-# tags_ct = word_count_array.select  { |word| word[0, 1] == "#" } 
-# puts "Tags: #{tags_ct.length}"
+# db.execute("INSERT INTO captions (caption, words, tags, words_ct, tags_ct) VALUES (#{user_input}, #{word_count_array}, #{tag_count_array}, #{words_ct}, #{tags_ct})")
 
-# delete periods
-# word_count_array.delete(-1)  { |char| char[-1] == "." } 
+
+# add a new caption
+p db
+
+puts "// This application will analyze your Instagram captions.\n//Please paste your Instagram caption here:\n"
+word_count(db, gets.chomp)
+
+
+# def create_kitten(db, name, age)
+#   db.execute("INSERT INTO kittens (name, age) VALUES (?, ?)", [name, age])
+# end
+
+# create_caption(db, caption)
+
+
+# db.execute("INSERT INTO captions (caption, words, tags, words_ct, tags_ct) VALUES (?,?,?,?,?)", [user_input, word_count_array, tag_count_array, words_ct, tags_ct])
+
+# count occurrences of a particular tag
+# display last 3 captions
+# tabulate top 10 tags
+
+
+
+
+
+
+
